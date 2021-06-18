26aa95d5e16c5c537130febebd5071b16fc7e217
StefanLegat <77675743+StefanLegat@users.noreply.github.com>
2021-06-18 11:21:30 +0200

    New Player added

--- myTeam/src/pkgMain/MainAppl.java
+++ myTeam/src/pkgMain/MainAppl.java
@@ -7,6 +7,7 @@ public class MainAppl {
 	public static void main(String[] args) {
 		Player p = new Player(1,"Dragovic","Defensive");
 		System.out.println(" == " + " " + p);
+		p = new Player(2,"Spieler 2", "Attack");
 
 	}
 
